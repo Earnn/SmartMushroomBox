d81091b7c7c3094bee2959ef6c799a42e78145f0
EARN <earnlune@gmail.com>
2017-07-10 20:13:43 +0700

    create register and login

---
+++ login/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls import url
+from . import views
+
+from django.contrib.auth import views as auth_views
+
+
+
+urlpatterns = [
+
+    url(r'^$', views.home, name='home'),
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+    url(r'^logout/$', auth_views.login, name='logout'),
+    url(r'^signup/$', views.signup, name='signup'),
+    # url(r'import_rule/', views.import_rule, name='import_rule'),
+    # url(r'accountlist/', AccountListView.as_view()),
+]
+# url(r'^$', core_views.home, name='home'),
+#     url(r'^login/$', auth_views.login, {'template_name': 'login.html'}, name='login'),
+#     url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+#     url(r'^signup/$', core_views.signup, name='signup'),

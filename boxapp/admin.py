d476df19bca4c9500b1e9adeea06502b879233a5
EARN <earnlune@gmail.com>
2017-07-22 10:23:45 +0700

    authen box and link pages

--- boxapp/admin.py
+++ boxapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class BoxAdmin(admin.ModelAdmin):
+	list_display=[f.name for f in Box._meta.fields]
+admin.site.register(Box, BoxAdmin)
